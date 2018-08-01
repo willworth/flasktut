8ed0b3085fdc5ab69da131ffc9a6d444d0167bfc
Will Worth <will.worth@gmail.com>
2018-08-01 23:23:53 +0200

    template inherit

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,5 +4,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Will'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
